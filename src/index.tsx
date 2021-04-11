9613a920eae75c670bb77191296aeda97cc22e3c
Анатолий Золотухин <anatolii.zolotukhin@stecpoint.ru>
2021-04-11 21:28:16 +0700

    global css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import './_assets/css/global.css';
+
 const App = () => <div>Test</div>;
 
 render(<App />, document.querySelector('#app'));
